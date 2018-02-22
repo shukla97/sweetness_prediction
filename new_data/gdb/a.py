b028b0412d032ace787a61f35d5756635f20cd97
shukla97 <shubhamshkl270@gmail.com>
2018-02-23 01:20:19 +0530

    final project

--- new_data/gdb/a.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("gdb_data.csv")
-k = 0
-for i in df["smiles"]:
-    k = k+1
-    z = str(k)
-    file= open(z + ".smi","w")
-    file.write(str(i))
-    file.close()
